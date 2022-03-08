0d6287259295e70819631aa9e953f1d6d873967c
ALAN PAUL VARGHESE <alanpv99@gmail.com>
2022-03-08 21:18:29 +0530

    added base stack class

---
+++ stack/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Stack{
+
+int* a;
+int max_size;
+int current_size;
+public:
+    Stack(int val){
+     this->a=new int[val];
+     this->max_size=0;
+     this->current_size=0;
+    }
+
+   void push(int x){
+
+   }
+
+};
+
+
+
+int main()
+{
+    cout << (9<9);
+    return 0;
+}
